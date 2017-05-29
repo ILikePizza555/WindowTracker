32fc5dce50ccf768f45d7e68448aa1906de13dd7
Isaac Avram <avrisaac555@gmail.com>
2017-05-28 21:44:44 -0700

    FInished the GUI

--- WindowTracker/UI/MainWindow.xaml.cs
+++ WindowTracker/UI/MainWindow.xaml.cs
@@ -21,17 +21,17 @@ namespace WindowTracker.UI
 
     public class DataEvent
     {
-        public DateTime Time;
-        public String Name;
-        public uint ThreadId;
+        public DateTime Time { get; set; }
+        public String Name { get; set; }
+        public uint ThreadId { get; set; }
     }
 
     public class DataProcess
     {
         public uint CurrentPid { get; set; }
         public string CurrentName { get; set;  }
-        public List<DataEvent> Events = new List<DataEvent>();
-        public List<String> DllList = new List<string>();
+        public ObservableCollection<DataEvent> Events = new ObservableCollection<DataEvent>();
+        public ObservableCollection<String> DllList = new ObservableCollection<String>();
     }
 
     /// <summary>
@@ -46,10 +46,30 @@ namespace WindowTracker.UI
         public MainWindow()
         {
             InitializeComponent();
-            EventDelegate = new WinEventDelegate(EventHandler);
+            EventDelegate = new WinEventDelegate(Win32EventHandler);
         }
 
-        void EventHandler(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        private void ProcessList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = sender as DataGrid;
+            DataProcess selectedDataProcess = dataGrid?.SelectedItem as DataProcess;
+
+            if (selectedDataProcess == null)
+            {
+                EventList.ItemsSource = null;
+                DllList.ItemsSource = null;
+            }
+            else
+            {
+                EventList.ItemsSource = selectedDataProcess.Events;
+                DllList.ItemsSource = selectedDataProcess.DllList;
+            }
+
+            EventList.Items.Refresh();
+            DllList.Items.Refresh();
+        }
+
+        void Win32EventHandler(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
         {
             //Only want Window Events
             if (idObject != 0 || idChild != 0)
@@ -65,6 +85,9 @@ namespace WindowTracker.UI
             {
                 Process p = Process.GetProcessById((int) processId);
 
+                //Check to see if a process with the same name exists. If it does update it. 
+                //If it doesn't, add it.
+                //TODO: Get a better way of getting the processes.
                 DataProcess d = Processes.FirstOrDefault(i => i.CurrentPid == processId);
                 if (d != null)
                 {
@@ -72,11 +95,21 @@ namespace WindowTracker.UI
                 }
                 else
                 {
-                    Processes.Add(new DataProcess{
+                    d = new DataProcess
+                    {
                         CurrentPid = processId,
                         CurrentName = p.MainModule.ModuleName
-                    });
+                    };
+                    Processes.Add(d);
                 }
+
+                if (eventType == User32.EVENT_OBJECT_DESTROY)
+                    d.Events.Add(new DataEvent{Name = "EVENT_OBJECT_DESTROY", ThreadId = dwEventThread, Time = DateTime.Now});
+                else if (eventType == User32.EVENT_OBJECT_CREATE)
+                    d.Events.Add(new DataEvent { Name = "EVENT_OBJECT_CREATE", ThreadId = dwEventThread, Time = DateTime.Now });
+
+                d.DllList.Clear();
+                foreach (ProcessModule pModule in p.Modules) d.DllList.Add(pModule.ModuleName);
             }
             catch (ArgumentException e)
             {
